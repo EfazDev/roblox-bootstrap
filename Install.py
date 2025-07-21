426fcf1a3d9a988eb92a3a8a3e4851ce8ffd53b7
EfazDev <me@efaz.dev>
2025-07-21 03:58:51 -0500

    install

--- Install.py
+++ Install.py
@@ -4,4 +4,4 @@ import sys
 import os
 current_path_location = os.path.dirname(os.path.abspath(__file__))
 if platform.system() == "Windows": subprocess.run([sys.executable, "Main.py"], cwd=current_path_location, shell=False)
-else: subprocess.run([sys.executable, "Main.py", "-s"], cwd=current_path_location, shell=False)
+elif platform.system() == "Darwin" and not ("--update-mode" in sys.argv): subprocess.run([sys.executable, "Main.py", "-s"], cwd=current_path_location, shell=False)
